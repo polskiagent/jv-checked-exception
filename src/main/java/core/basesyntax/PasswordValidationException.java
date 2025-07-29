a3bbb244121e6c05ce2557fd2fb392dab067e250
Piotr Seredynski <sersoft14@gmail.com>
2025-07-29 07:46:35 +0200

    Finish project

--- src/main/java/core/basesyntax/PasswordValidationException.java
+++ src/main/java/core/basesyntax/PasswordValidationException.java
@@ -1,3 +1,8 @@
 package core.basesyntax;
 
+public class PasswordValidationException extends Exception {
+    public PasswordValidationException(String wrongPasswords) {
+        super(wrongPasswords);
+    }
+}
 //write your code here
